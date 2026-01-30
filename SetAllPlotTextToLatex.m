e877069128c84a389462298a2db1f6bb4f06b39f
Jonathan Hargreaves <j.a.hargreaves@salford.ac.uk>
2026-01-30 10:28:50 +0000

    Added SetAllPlotTextToLatex.m
    Sets Matlab's root graphic preferences so that all plot text interpretation defaults to LaTeX

---
+++ SetAllPlotTextToLatex.m
@@ -0,0 +1,7 @@
+
+% Change plotting labels to use LaTeX:
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(groot, 'defaultGraphplotInterpreter', 'latex');
+set(groot, 'defaultTextInterpreter', 'latex');
+
